4f943facdbe44e189100787212100c6fa4cb58f5
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-02-10 17:47:14 +0900

    :memo: BOJ_14502 업로드

--- week1-io/BOJ_14502/석희.java
+++ week1-io/BOJ_14502/석희.java
@@ -1,5 +1,24 @@
-public class BOJ_14502 {
-    public static void main(String[] args) {
-        // 폴더 구조 생성을 위한 임시 파일
+import java.util.*;
+
+public class BOJ_14502 /* 석희 */ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int map[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println(Arrays.toString(map[i]));
+        }
     }
 }
