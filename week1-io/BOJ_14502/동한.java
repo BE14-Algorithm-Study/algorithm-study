ddebae707797c372fd373ac3b4585458be992f0b
Donghan <98398484+rainyday1367@users.noreply.github.com>
2025-02-11 23:49:17 +0900

    Create 동한.java

---
+++ week1-io/BOJ_14502/동한.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj14502 // 동한 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < arr[i].length; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+}
