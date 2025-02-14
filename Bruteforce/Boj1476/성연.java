454857f8b1ce9122cf6c0a65516f46389c2c22bc
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-14 21:36:02 +0900

    Create 성연.java

---
+++ Bruteforce/Boj1476/성연.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        boolean[] d = new boolean[10001]; //1~10000 삭제된 값 확인용.
+
+        for (int i = 1; i < 10001; i++) {
+            int n = i;
+            int j = i;
+
+            while (j > 0) {
+                n += j % 10;
+                j /= 10;
+            }
+
+            if (n < 10001) {
+                d[n] = true;
+            }
+        }
+        for (int i = 1; i < 10001; i++) {
+            if (!d[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+        
+        System.out.println(sb);
+    }
+}
