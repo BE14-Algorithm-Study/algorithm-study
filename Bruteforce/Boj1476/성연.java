ac4c3a7a226cc8ee8a04e6b9f84576b81d9ff378
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-14 21:55:48 +0900

    Update 성연.java

--- Bruteforce/Boj1476/성연.java
+++ Bruteforce/Boj1476/성연.java
@@ -1,32 +1,40 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        boolean[] d = new boolean[10001]; //1~10000 삭제된 값 확인용.
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i < 10001; i++) {
-            int n = i;
-            int j = i;
+        int earth = Integer.parseInt(st.nextToken());
+        int sun = Integer.parseInt(st.nextToken());
+        int moon = Integer.parseInt(st.nextToken());
 
-            while (j > 0) {
-                n += j % 10;
-                j /= 10;
-            }
+        int curEarth = 1;
+        int curSun = 1;
+        int curMoon = 1;
 
-            if (n < 10001) {
-                d[n] = true;
-            }
+        int year = 1;
+
+        while(earth != curEarth || sun != curSun || moon != curMoon) { //세개 다 동일할때까지 반복
+            curEarth = initializationYear(curEarth, 15);
+            curSun = initializationYear(curSun, 28);
+            curMoon = initializationYear(curMoon, 19);
+            year++;
         }
-        for (int i = 1; i < 10001; i++) {
-            if (!d[i]) {
-                sb.append(i).append("\n");
-            }
-        }
-        
+
+        sb.append(year);
         System.out.println(sb);
+
+    }
+
+    private static int initializationYear(int planet, int resetYear) { //각 행성별 날짜초기화.
+        planet++;
+        if(planet == resetYear + 1) {
+            planet = 1;
+        }
+
+        return planet;
     }
 }
